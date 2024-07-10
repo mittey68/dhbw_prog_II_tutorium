88d25bde85882dfe4331f257731aab1a9e67c15c
Alexander <33837034+mittey68@users.noreply.github.com>
2024-07-10 18:54:06 +0200

    Comment overhaul

--- Termin 1/src/klausur/aufgabe5/MobilePhone.java
+++ Termin 1/src/klausur/aufgabe5/MobilePhone.java
@@ -4,18 +4,24 @@ public class MobilePhone extends Phone {
     // -os: OS {final} aus UML-Diagramm
     private final OS os;
 
-    // +MobilePhone(description: String, brand: Brand, valueInEuro: double, os: OS) aus UML-Diagramm
+    /*
+     * +MobilePhone(description: String, brand: Brand, valueInEuro: double, os: OS)
+     * aus UML-Diagramm
+     */
     public MobilePhone(String description, Brand brand, double valueInEuro, OS os) {
-        // Aufruf des Konstruktors der Oberklasse, um description, brand und valueInEuro zu setzen. Diese Attribute können nicht direkt in der Unterklasse gesetzt werden.
-        // super-Konstruktor muss immer als erstes in der Konstruktor-Implementierung aufgerufen werden
-        super(description, brand, valueInEuro); 
+        /*
+         * Aufruf des Konstruktors der Oberklasse, um description, brand und valueInEuro
+         * zu setzen. Diese Attribute können nicht direkt in der Unterklasse gesetzt
+         * werden. super-Konstruktor muss immer als erstes in der
+         * Konstruktor-Implementierung aufgerufen werden
+         */
+        super(description, brand, valueInEuro);
         // Setzen des os-Attributs der Unterklasse
-        this.os = os; 
+        this.os = os;
     }
 
-    // +os(): OS aus UML-Diagramm
-    // Getter-Methode für das os-Attribut
-    public OS os() { 
-        return os; 
+    /* +os(): OS aus UML-Diagramm und die Getter-Methode für das os-Attribut */
+    public OS os() {
+        return os;
     }
 }
