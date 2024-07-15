2c31fc1976d0d21b2665e832d1aa3ea58e6ccb04
Alexander <33837034+mittey68@users.noreply.github.com>
2024-07-15 20:09:05 +0200

    Answered Question about lists

--- Termin 2/src/besprechungen/lists/Lists.java
+++ Termin 2/src/besprechungen/lists/Lists.java
@@ -1,21 +1,29 @@
 package besprechungen.lists;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Lists {
     public static void main(String[] args) {
         /*
-         * Sowohl Zeile 14 als auch Zeile 15 ergeben ausführbaren korrekten Code. Wenn man
-         * sich jedoch im NAchhinein umetscheiden sollte, welche Art von Liste man
-         * verwenden möchte (siehe Termin 1/src/besprechungen/lists), ist dies in Zeile 18 aufgrund von Ploymorphie möglich, in Zeile 15 allerdings nicht.
+         * Sowohl Zeile 14 als auch Zeile 15 ergeben ausführbaren korrekten Code. Wenn
+         * man sich jedoch im NAchhinein umetscheiden sollte, welche Art von Liste man
+         * verwenden möchte (siehe Termin 1/src/besprechungen/lists), ist dies in Zeile
+         * 18 möglich, in Zeile 15 allerdings nicht.
          */
-        private List<String> list;
-        private ArrayList<String> arrayList;
+        List<String> list;
+        ArrayList<String> arrayList;
 
+        /*
+         * Kein Fehler, da ArrayList eine Liste ist und somit in eine Liste gecastet
+         * werden kann
+         */
         list = List.of("Hallo", "Welt");
-        arrayList =  List.of("Hallo", "Welt");
+        /*
+         * Fehler, da List.of() eine unveränderliche Liste zurückgibt und keine
+         * ArrayList
+         */
+        // arrayList = List.of("Hallo", "Welt");
     }
 
 }
