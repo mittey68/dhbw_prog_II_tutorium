844fa593ebedceafb5277963452eabc7a5ae73bc
Alexander <33837034+mittey68@users.noreply.github.com>
2024-08-13 10:57:28 +0200

    Insert Planets excercise

---
+++ Termin5/src/Streams/Planets/Planet.java
@@ -0,0 +1,5 @@
+package Termin5.src.Streams.Planets;
+
+public record Planet(String name, double diameter, double mass, int moons, Type type) {
+
+}
---
+++ Termin5/src/Streams/Planets/Type.java
@@ -0,0 +1,7 @@
+package Termin5.src.Streams.Planets;
+
+public enum Type {
+
+    GAS_PLANET, TERRESTRIAL_PLANET, DWARF_PLANET;
+
+}
